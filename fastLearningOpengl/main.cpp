7cbb2bcefceb8e5d2be0f455e90e1ef499383a47
Quang Vu <131584933+mentallyderanged@users.noreply.github.com>
2025-05-12 23:50:47 +0700

    ??? what is going on

--- fastLearningOpengl/main.cpp
+++ fastLearningOpengl/main.cpp
@@ -2,17 +2,58 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source){
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE){
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+};
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader){
+    // taking in the actual source code of these shaders as strings.
+
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 int main(){
 
     glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing (MSAA)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+
+    // create window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "red triangle", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -35,6 +76,10 @@ int main(){
          0.0f,  0.5f, 0.0f
     };
 
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -44,15 +89,37 @@ int main(){
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    //shader
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n" // <-- location is the index of the attribute in the vertex data  :3
+        "void main()\n"
+        "{\n"
+        "gl_Position = position;\n"
+        "}\n";
 
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" // rgba
+        "}\n";
+
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+    
     // render loops
     while(!glfwWindowShouldClose(window)){
         processInput(window);
 
         // rendering command
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // <-- state setting function
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // <-- state setting function
         glClear(GL_COLOR_BUFFER_BIT); // <-- state using function
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // event checks n calls; buffer swap
         glfwSwapBuffers(window);
